63dd9244ce2bb0230f200056bc3a575f410d110b
Sofonyas <sofstzz@gmail.com>
2025-02-04 19:40:31 +0300

    models fix

--- backend/infrastructure/doc/document.go
+++ backend/infrastructure/doc/document.go
@@ -3,24 +3,22 @@ package service
 import (
 	"context"
 	"fmt"
+	"log"
 
-	// "errors"
-	// "fmt"
-	"github.com/sofc-t/code_pulse/dto"
-
+	models "github.com/sofc-t/code_pulse/models"
 	"go.mongodb.org/mongo-driver/bson"
 	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
 type Documentrepo interface {
-	GetAllDocuments(email dto.Email) ([]*dto.Document, error)
-	SearchDocuments(email string, searchQuery string) ([]*dto.Document, error)
+	GetAllDocuments(email models.Email) ([]*models.Document, error)
+	SearchDocuments(email string, searchQuery string) ([]*models.Document, error)
 	CreateDocument(author string, title string, body interface{}, readAccess []string, writeAccess []string) (string, error)
-	UpdateDocument(documentID string, body dto.DocumentData) error
-	GetDocumentByID(documentID string) (*dto.Document, error)
+	UpdateDocument(documentID string, body models.DocumentData) error
+	GetDocumentByID(documentID string) (*models.Document, error)
 	UpdateTitle(documentID string, title string) (string, error)
-	UpdateCollaborators(documentID string, collaborators dto.Access) (dto.Document, error)
+	UpdateCollaborators(documentID string, collaborators models.Access) (models.Document, error)
 	DeleteDocument(documentID string) (bool, error)
 }
 
@@ -35,31 +33,35 @@ func NewDocumentrepo(client *mongo.Client, databaseName, collectionName string) 
 	}
 }
 
-func (repo *documentrepo) GetAllDocuments(email dto.Email) ([]*dto.Document, error) {
+func (repo *documentrepo) GetAllDocuments(email models.Email) ([]*models.Document, error) {
+	log.Printf("Getting all documents for email: %s", email.Email)
 	filter := bson.M{"$or": []bson.M{{"author": email.Email}, {"readAccess": email.Email}, {"writeAccess": email.Email}}}
 	cursor, err := repo.collection.Find(context.Background(), filter)
 	if err != nil {
+		log.Printf("Error finding documents: %v", err)
 		return nil, err
 	}
 	defer cursor.Close(context.Background())
 
-	var documents []*dto.Document
+	var documents []*models.Document
 	for cursor.Next(context.Background()) {
-		var document dto.Document
+		var document models.Document
 		if err := cursor.Decode(&document); err != nil {
+			log.Printf("Error decoding document: %v", err)
 			return nil, err
 		}
-		documentDTO := &dto.Document{
+		documentmodels := &models.Document{
 			ID:    document.ID,
 			Title: document.Title,
 		}
 
-		documents = append(documents, documentDTO)
+		documents = append(documents, documentmodels)
 	}
 	return documents, nil
 }
 
-func (repo *documentrepo) SearchDocuments(email string, searchQuery string) ([]*dto.Document, error) {
+func (repo *documentrepo) SearchDocuments(email string, searchQuery string) ([]*models.Document, error) {
+	log.Printf("Searching documents for email: %s with query: %s", email, searchQuery)
 	filter := bson.M{
 		"$or": []bson.M{
 			{"author": email},
@@ -73,28 +75,30 @@ func (repo *documentrepo) SearchDocuments(email string, searchQuery string) ([]*
 	}
 	cursor, err := repo.collection.Find(context.Background(), filter)
 	if err != nil {
+		log.Printf("Error finding documents: %v", err)
 		return nil, err
 	}
 	defer cursor.Close(context.Background())
 
-	var documents []*dto.Document
+	var documents []*models.Document
 	for cursor.Next(context.Background()) {
-		var document dto.Document
+		var document models.Document
 		if err := cursor.Decode(&document); err != nil {
+			log.Printf("Error decoding document: %v", err)
 			return nil, err
 		}
-		documentDTO := &dto.Document{
+		documentmodels := &models.Document{
 			ID:    document.ID,
 			Title: document.Title,
 		}
 
-		documents = append(documents, documentDTO)
+		documents = append(documents, documentmodels)
 	}
 	return documents, nil
 }
 
 func (repo *documentrepo) CreateDocument(author string, title string, body interface{}, readAccess []string, writeAccess []string) (string, error) {
-	// Implement logic to create a document in the MongoDB collection
+	log.Printf("Creating document for author: %s with title: %s", author, title)
 	newDocument := bson.D{
 		{Key: "author", Value: author},
 		{Key: "readAccess", Value: readAccess},
@@ -105,40 +109,49 @@ func (repo *documentrepo) CreateDocument(author string, title string, body inter
 
 	document, err := repo.collection.InsertOne(context.Background(), newDocument)
 	if err != nil {
+		log.Printf("Error inserting document: %v", err)
 		return "", err
 	}
 
 	insertedID, ok := document.InsertedID.(primitive.ObjectID)
 	if !ok {
+		log.Printf("Failed to convert InsertedID to string")
 		return "", fmt.Errorf("failed to convert InsertedID to string")
 	}
 
 	return insertedID.Hex(), nil
 }
 
-func (repo *documentrepo) UpdateDocument(documentID string, incomingData dto.DocumentData) error {
+func (repo *documentrepo) UpdateDocument(documentID string, incomingData models.DocumentData) error {
+	log.Printf("Updating document with ID: %s", documentID)
 	objectID, err := primitive.ObjectIDFromHex(documentID)
 	if err != nil {
+		log.Printf("Error converting documentID to ObjectID: %v", err)
 		return err
 	}
 
 	update := bson.M{"$set": bson.M{"data.ops": incomingData.Ops}}
 	filter := bson.M{"_id": objectID}
 	_, err = repo.collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Printf("Error updating document: %v", err)
+	}
 	return err
 }
 
-func (repo *documentrepo) GetDocumentByID(documentID string) (*dto.Document, error) {
-	// Implement logic to fetch a document by its ID from the MongoDB collection
-	var document dto.Document
+func (repo *documentrepo) GetDocumentByID(documentID string) (*models.Document, error) {
+	log.Printf("Getting document by ID: %s", documentID)
+	var document models.Document
 	objectID, err := primitive.ObjectIDFromHex(documentID)
 	if err != nil {
+		log.Printf("Error converting documentID to ObjectID: %v", err)
 		return nil, err
 	}
 
 	filter := bson.M{"_id": objectID}
 	err = repo.collection.FindOne(context.Background(), filter).Decode(&document)
 	if err != nil {
+		log.Printf("Error finding document: %v", err)
 		return nil, err
 	}
 
@@ -146,22 +159,27 @@ func (repo *documentrepo) GetDocumentByID(documentID string) (*dto.Document, err
 }
 
 func (repo *documentrepo) UpdateTitle(documentID string, title string) (string, error) {
-	// Implement logic to update the title of a document in the MongoDB collection
+	log.Printf("Updating title of document with ID: %s to: %s", documentID, title)
 	objectID, err := primitive.ObjectIDFromHex(documentID)
 	if err != nil {
+		log.Printf("Error converting documentID to ObjectID: %v", err)
 		return "", err
 	}
 	filter := bson.M{"_id": objectID}
 	update := bson.M{"$set": bson.M{"title": title}}
 	_, err = repo.collection.UpdateOne(context.Background(), filter, update)
+	if err != nil {
+		log.Printf("Error updating title: %v", err)
+	}
 	return "", err
 }
 
-func (repo *documentrepo) UpdateCollaborators(documentID string, collaborators dto.Access) (dto.Document, error) {
+func (repo *documentrepo) UpdateCollaborators(documentID string, collaborators models.Access) (models.Document, error) {
+	log.Printf("Updating collaborators of document with ID: %s", documentID)
 	objectID, err := primitive.ObjectIDFromHex(documentID)
 	if err != nil {
-		fmt.Print(err)
-		return dto.Document{}, err
+		log.Printf("Error converting documentID to ObjectID: %v", err)
+		return models.Document{}, err
 	}
 
 	filter := bson.M{"_id": objectID}
@@ -170,27 +188,32 @@ func (repo *documentrepo) UpdateCollaborators(documentID string, collaborators d
 	// Perform the update operation
 	_, err = repo.collection.UpdateOne(context.Background(), filter, update)
 	if err != nil {
-		return dto.Document{}, err
+		log.Printf("Error updating collaborators: %v", err)
+		return models.Document{}, err
 	}
 
 	// Retrieve the updated document
-	var updatedDocument dto.Document
+	var updatedDocument models.Document
 	err = repo.collection.FindOne(context.Background(), filter).Decode(&updatedDocument)
 	if err != nil {
-		return dto.Document{}, err
+		log.Printf("Error finding updated document: %v", err)
+		return models.Document{}, err
 	}
 	return updatedDocument, nil
 }
 
 func (repo *documentrepo) DeleteDocument(documentID string) (bool, error) {
+	log.Printf("Deleting document with ID: %s", documentID)
 	objectID, err := primitive.ObjectIDFromHex(documentID)
 	if err != nil {
+		log.Printf("Error converting documentID to ObjectID: %v", err)
 		return false, err
 	}
 
 	filter := bson.M{"_id": objectID}
 	result, err := repo.collection.DeleteOne(context.Background(), filter)
 	if err != nil {
+		log.Printf("Error deleting document: %v", err)
 		return false, err
 	}
 
